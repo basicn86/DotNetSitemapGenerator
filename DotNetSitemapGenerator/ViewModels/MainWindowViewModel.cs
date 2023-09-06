9d6c4061aa3ddab9a8eb887729ca0f2cbbf2b5ba
Nedim Basic <basicn86@gmail.com>
2023-09-05 22:46:46 -0400

    Added save directory.

--- DotNetSitemapGenerator/ViewModels/MainWindowViewModel.cs
+++ DotNetSitemapGenerator/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,34 @@
 ﻿using DotNetSitemapGenerator.Utilities;
 using System;
 using System.Threading.Tasks;
+using Avalonia.Dialogs;
+using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+using System.IO;
+using System.Linq;
 
 namespace DotNetSitemapGenerator.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
         public string RequestedURL { get; set; } = "http://dev.localhost";
-        public CurrentOperationViewModel CurrentOperationViewModel { get; } = new CurrentOperationViewModel();
+        private CurrentOperationViewModel _CurrentOperationViewModel { get; }
+        private SaveDirectoryViewModel _SaveDirectoryViewModel { get; }
+
+        public CurrentOperationViewModel CurrentOperationViewModel
+        {
+            get { return _CurrentOperationViewModel; }
+        }
+        public SaveDirectoryViewModel SaveDirectoryViewModel
+        {
+            get { return _SaveDirectoryViewModel; }
+        }
+
+        public MainWindowViewModel()
+        {
+            _CurrentOperationViewModel = new CurrentOperationViewModel();
+            _SaveDirectoryViewModel = new SaveDirectoryViewModel();
+        }
 
         public async void ButtonClick()
         {
@@ -15,10 +36,35 @@ namespace DotNetSitemapGenerator.ViewModels
             try
             {
                 WebPage page = await HttpDownloader.DownloadStringAsync(new Uri(RequestedURL));
-                CurrentOperationViewModel.CurrentOperation = "Finished!" + RequestedURL;
+                CurrentOperationViewModel.CurrentOperation = "Finished downloading!";
+
+                CurrentOperationViewModel.CurrentOperation = "Saving file...";
+                StreamWriter writer = new StreamWriter(SaveDirectoryViewModel.SaveDirectory, false);
+                await writer.WriteAsync(page.Content);
+                writer.Close();
+                CurrentOperationViewModel.CurrentOperation = "File saved!";
             } catch (Exception ex)
             {
                 CurrentOperationViewModel.CurrentOperation = "Error: " + ex.Message;
+            }
+        }
+
+        public async void SaveFile()
+        {
+            var dialog = new SaveFileDialog()
+            {
+                Title = "Save Sitemap",
+                InitialFileName = "sitemap.xml",
+                DefaultExtension = "xml"
+            };
+
+            //open the dialog
+            var result = await dialog.ShowAsync(new Window());
+
+            if (result != null)
+            {
+                SaveDirectoryViewModel.SaveDirectory = result;
+                CurrentOperationViewModel.CurrentOperation = "Saving to " + result;
             }
         }
     }
---
+++ DotNetSitemapGenerator/ViewModels/SaveDirectoryViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetSitemapGenerator.ViewModels
+{
+    public class SaveDirectoryViewModel : INotifyPropertyChanged
+    {
+        //create event handler
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        //raise the event
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string? _SaveDirectory;
+
+        public string? SaveDirectory
+        {
+            get
+            {
+                return _SaveDirectory;
+            }
+
+            set
+            {
+                if (_SaveDirectory != value)
+                {
+                    _SaveDirectory = value;
+
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public SaveDirectoryViewModel()
+        {
+            SaveDirectory = "";
+        }
+    }
+}
